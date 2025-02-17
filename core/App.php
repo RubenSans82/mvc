432fa9954fa225f4ce6986d60b17a5c883cb43ab
RubenSans82 <rubensans82@gmail.com>
2025-02-17 17:05:11 +0100

    second step

--- core/App.php
+++ core/App.php
@@ -1,18 +1,24 @@
 <?php
-class App
-{
-    protected $controller = "HomeController";
+class App{
+    protected $controller="HomeController";
     protected $method;
-    protected $params = [];
+    protected $params=[];
 
-    public function __construct()
-    {
-
+    public function __construct(){
+        $url=$this->parseUrl();
+               
+         // Verificar el controlador
+         if(file_exists('../app/controllers/' . ucfirst($url[0]) . 'Controller.php')) {
+            $this->controller = ucfirst($url[0]) . 'Controller';
+            unset($url[0]);
+        }
+        require_once '../app/controllers/' . $this->controller . '.php';
+        $this->controller = new $this->controller;
     }
+    
 
-    private function parseUrl()
-    {
-        if (isset($_GET['url'])) {
+    private function parseUrl() {
+        if(isset($_GET['url'])) {
             return explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
         }
         return ['home'];
